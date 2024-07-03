64019120f24e51bb24528746af3b71a8c792a01e
Giovanna de Freitas <giovannadefreitasaraujo@hotmail.com>
2024-07-03 17:41:52 -0300

    .

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function HomePage() {
+    return (
+        <View style={{ flex: 1}}>
+            <Text>teste fdp indx index index index index</Text>
+        </View>
+    )
+}
